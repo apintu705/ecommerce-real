04d3aa776228c35e4695f91b9b8fe421ad8a38b8
Abhishek Kumar <93374250+apintu705@users.noreply.github.com>
2024-05-17 17:32:41 +0530

    Update server.js

--- backend/backend/server.js
+++ backend/backend/server.js
@@ -19,7 +19,11 @@ require("dotenv").config();
 const app=express();
 app.use(express.json());
 const port =process.env.PORT || 5000;
-app.use(cors())
+app.use(cors({
+  origin: 'https://localhost:3000/',
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
 app.use(express.json())
 
 
